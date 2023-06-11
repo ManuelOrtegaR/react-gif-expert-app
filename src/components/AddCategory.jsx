3381e29259ff91cafde91db8b005a652db60aa7b
Manuel Ortega Ruiz <manuellortegaruiz@gmail.com>
2023-06-11 17:18:33 -0500

    first commit

---
+++ src/components/AddCategory.jsx
@@ -0,0 +1,24 @@
+import { useState } from 'react';
+
+export const AddCategory = ({ onNewCategory }) => {
+  const [inputValue, setInputValue] = useState('');
+  const onInputChange = ({ target }) => {
+    setInputValue(target.value);
+  };
+  const onSubmit = (event) => {
+    event.preventDefault();
+    if (inputValue.trim().length <= 1) return;
+    setInputValue('');
+    onNewCategory(inputValue.trim());
+  };
+  return (
+    <form onSubmit={onSubmit}>
+      <input
+        type='text'
+        placeholder='¿Qué quieres buscar?'
+        value={inputValue}
+        onChange={onInputChange}
+      />
+    </form>
+  );
+};
---
+++ src/helpers/getGifs.js
@@ -0,0 +1,13 @@
+const APIKey = 'L10o9oxH4rgrgghQ3gmf9Z5CvEe4fmwp';
+
+export const getGifs = async (category) => {
+  const url = `https://api.giphy.com/v1/gifs/search?api_key=${APIKey}&q=${category}&limit=10`;
+  const resp = await fetch(url);
+  const { data = [] } = await resp.json();
+  const gifs = data.map((img) => ({
+    id: img.id,
+    title: img.title,
+    url: img.images.downsized_medium.url,
+  }));
+  return gifs;
+};
